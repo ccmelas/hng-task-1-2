1cd319122d43f0f6c7ce6ddb72e88f9e1247f70b
Chiemela Chinedum <chiemelachinedum@gmail.com>
2018-04-09 08:19:31 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Challenge 1</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="images/favicon.png">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <div class="container">
+        <nav>
+            <div class="nav-header">
+                HNG TASK 2
+            </div>
+            <div class="nav-items">
+                <div class="nav-item has-separator">
+                    <a href="https://hnginternship4.slack.com" target="_blank" class="nav-item-link">SLACK</a>
+                </div>
+                <div class="nav-item has-separator">
+                    <a href="https://google.com" target="_blank" class="nav-item-link">GOOGLE</a>
+                </div>
+                <div class="nav-item">
+                    <a href="https://twitter.com/ccmelas" target="_blank" class="nav-item-link">TWITTER</a>
+                </div>
+            </div>
+        </nav>
+    
+        <section class="timer">
+            <div class="time">
+                <?php echo date('h:i: A'); ?>
+            </div>
+        </section>
+    </div>
+</body>
+</html>
